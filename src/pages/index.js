2a33f6bb7971e7322b79cae229ac4dc3c9eff667
Harshna Goratela <harshna.goratela@gmail.com>
2020-03-10 11:21:54 +0530

    index page cms integration completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,10 @@ import Helmet from 'react-helmet'
 import Layout from '../components/Layout'
 import Banner from '../components/Banner'
 
-import pic01 from '../assets/images/pic01.jpg'
-import pic02 from '../assets/images/pic02.jpg'
-import pic03 from '../assets/images/pic03.jpg'
-import pic04 from '../assets/images/pic04.jpg'
-import pic05 from '../assets/images/pic05.jpg'
-import pic06 from '../assets/images/pic06.jpg'
-
 class HomeIndex extends React.Component {
     render() {
 
         const node = this.props.data.allFile.edges[0].node;
-
-        console.log(node)
 
         return (
             <Layout>
@@ -47,12 +38,9 @@ class HomeIndex extends React.Component {
                     <section id="two">
                         <div className="inner">
                             <header className="major">
-                                <h2>Massa libero</h2>
+                                <h2>{node.childMarkdownRemark.frontmatter.intro.heading}</h2>
                             </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
-                            <ul className="actions">
-                                <li><Link to="/landing" className="button next">Get Started</Link></li>
-                            </ul>
+                            <p>{node.childMarkdownRemark.frontmatter.intro.description}</p>
                         </div>
                     </section>
                 </div>
