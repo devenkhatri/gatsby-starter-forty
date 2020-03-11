23e6ffd42a7777ed5e50a6539709679e236b2b70
Deven Goratela <deven.goratela@tcs.com>
2020-03-11 16:16:49 +0530

    resolving banner landing error

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,19 +1,30 @@
 import React from 'react'
 
-const Banner = (props) => (
+const Banner = props => {
+  let styleName = 'style' + Math.floor(Math.random() * 6 + 1)
+  const heading = props.data.heading ? props.data.heading : 'Heading'
+  const subheading = props.data.subheading
+    ? props.data.subheading
+    : 'Short Description...'
+  return (
     <section id="banner" className="major">
-        <div className="inner">
-            <header className="major">
-                <h1>{props.data.heading}</h1>
-            </header>
-            <div className="content">
-                <p>{props.data.subheading}</p>
-                <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
-                </ul>
-            </div>
+      <div className="inner">
+        <header className="major">
+          <h1>{heading}</h1>
+        </header>
+        <div className="content">
+          <p>{subheading}</p>
+          <ul className="actions">
+            <li>
+              <a href="#one" className="button next scrolly">
+                Get Started
+              </a>
+            </li>
+          </ul>
         </div>
+      </div>
     </section>
-)
+  )
+}
 
 export default Banner
--- src/components/BannerLanding.js
+++ src/components/BannerLanding.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 const BannerLanding = (props) => {
     let styleName = "style"+(Math.floor(Math.random()*6+1))
+    const title = props.data.title ? props.data.title : "Heading"
+    const excerpt = props.data.excerpt ? props.data.excerpt : "Short Description..."
   return (
     <section id="banner" className={styleName}>
       <div className="inner">
         <header className="major">
-          <h1>{props.data.title}</h1>
+          <h1>{title}</h1>
         </header>
         <div className="content">
-          <p>{props.data.excerpt}</p>
+          <p>{excerpt}</p>
         </div>
       </div>
     </section>
