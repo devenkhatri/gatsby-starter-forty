3e5bed536134c20a91e8b155c246c0bbfa793eeb
Deven Goratela <deven.goratela@tcs.com>
2020-03-11 16:06:44 +0530

    resolving banner landing error

--- src/components/BannerLanding.js
+++ src/components/BannerLanding.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
-const BannerLanding = (props) => (
-    <section id="banner" className="style2">
-        <div className="inner">
-            <header className="major">
-                <h1>{props.data.title}</h1>
-            </header>
-            <div className="content">
-                <p>{props.data.excerpt}</p>
-            </div>
+const BannerLanding = (props) => {
+    let styleName = "style"+(Math.floor(Math.random()*6+1))
+  return (
+    <section id="banner" className={styleName}>
+      <div className="inner">
+        <header className="major">
+          <h1>{props.data.title}</h1>
+        </header>
+        <div className="content">
+          <p>{props.data.excerpt}</p>
         </div>
+      </div>
     </section>
-)
-
+  )
+}
 export default BannerLanding
